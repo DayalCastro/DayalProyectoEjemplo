3beec94b054285ddc50e962b0def818368b2ab1f
Dayal Castro <dayalcastro@gmail.com>
2023-03-29 23:04:45 -0500

    Primer commit de proyecto simple con Java Spring

---
+++ dayal-proyecto/src/main/java/com/dayal/proyecto/ejemplo/DayalProyectoEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.dayal.proyecto.ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayalProyectoEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayalProyectoEjemploApplication.class, args);
+	}
+
+}
---
+++ dayal-proyecto/src/test/java/com/dayal/proyecto/ejemplo/DayalProyectoEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayal.proyecto.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayalProyectoEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
