7133418e0b74347c73704b368bd4fcfb5a6e09ff
Dayal Castro <dayalcastro@gmail.com>
2023-03-29 23:02:49 -0500

    Primer commit, cargando app con Java Spring

---
+++ dayal-proyecto/src/main/java/com/dayal/proyecto/ejemplo/HolaMundoController.java
@@ -0,0 +1,16 @@
+package com.dayal.proyecto.ejemplo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+
+
+public class HolaMundoController {
+    @GetMapping("/hola")
+    public String saludar() {
+        return "Saludo cordial";
+    }
+}
